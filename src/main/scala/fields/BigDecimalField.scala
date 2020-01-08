ef33c9e990dd2a7f69e5b3d325be6ea114990076
FMC <franklin@peeriq.com>
2020-01-08 11:43:19 -0500

    fix compiler error

--- src/main/scala/fields/BigDecimalField.scala
+++ src/main/scala/fields/BigDecimalField.scala
@@ -5,3 +5,6 @@ case class BigDecimalField[A](f: BigDecimal) extends AnyVal {
   override def toString: String = f.toString
 
 }
+
+sealed trait OutstandingPrincipalBOP
+sealed trait OutstandingPrincipalEOP
